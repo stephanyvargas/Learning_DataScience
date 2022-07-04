f0de58d1ee359abdf49873f5e75463c5ed0c4e95
stephanyvargas <yostephy@hotmail.com>
2022-07-04 15:35:54 +0900

    conection with the server working for one entry

---
+++ WebScrapping_data/datascraping_TCI.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://www.tcichemicals.com/JP/en/p/A0001", headers = {"User-Agent"
+                        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"})
+
+soup = BeautifulSoup(response.content, "html.parser")
+
