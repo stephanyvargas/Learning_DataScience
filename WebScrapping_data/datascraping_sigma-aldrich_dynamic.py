aa27d43617dda03b901b24b70e64cc1cc00f10bf
stephanyvargas <yostephy@hotmail.com>
2022-07-15 12:15:43 +0900

    close the chrome web automatically

--- WebScrapping_data/datascraping_sigma-aldrich_dynamic.py
+++ WebScrapping_data/datascraping_sigma-aldrich_dynamic.py
@@ -7,17 +7,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 
+
+#Create a user that would inspect the webpage (needed to avoid <Access Denied>)
 options = Options()
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
 options.add_argument('user-agent={0}'.format(user_agent))
 
+#Load the Chrome driver and open Chromium in the back.
 driver = webdriver.Chrome(options=options, executable_path='/home/stephy/Selenium_driver_chrome/chromedriver_linux64/chromedriver')
-wait = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 10)
 action = ActionChains(driver)
 
+#A chrome tab will open and wait for the entire JavaScript to load.
 driver.get("https://www.sigmaaldrich.com/JP/en/product/sigma/v5754")
 html = driver.page_source
 print(html)
-#Login_Btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@class='pxc-fn-login']/a")))
 
-#action.move_to_element(Login_Btn).click().perform()
+#Once data has been fully loaded and scraped, close the chrome tab automatically.
+driver.close()
