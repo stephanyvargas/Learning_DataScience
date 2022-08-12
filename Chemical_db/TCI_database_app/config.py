e86e2bc54e35913ab48dae43ea49880fbca94b2f
stephanyvargas <yostephy@hotmail.com>
2022-08-12 13:55:36 +0900

    credentials for heroku app

---
+++ Chemical_db/TCI_database_app/config.py
@@ -0,0 +1,5 @@
+host = 'ec2-44-209-186-51.compute-1.amazonaws.com'
+port = 5432
+database = 'd296c2ha88tb3u'
+user = 'egctdtynwamivp'
+password = '3f3351b444c495acc2e985082472fca88818641df83f4eec2a2d2c7973f9d8a6'
