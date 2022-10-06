5f5f8c0aceb0f59d686e4c60734e5a3a55af23de
stephanyvargas <yostephy@hotmail.com>
2022-10-07 01:28:32 +0900

    get_available_products function, returns table succesfully

--- WebScrapping_data/sigma_aldrich/clean_sigmaaldrich_data.py
+++ WebScrapping_data/sigma_aldrich/clean_sigmaaldrich_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import json
 import re
 
@@ -21,18 +22,62 @@ def get_unique_code(lst, how_many):
     return code
 
 
+def get_available_products(lst, how_many, code):
+    products = []
+    for i in range(how_many):
+        for product in lst[i][code[i]]['Available_products']:
+            try:
+                quantity = re.findall(r'\d+',product['ID - quatity'])[-1]
+                if quantity and len(quantity) <= 5:
+                    amount = quantity
+                    unit = product['ID - quatity'].split(amount)[-1].strip()
+                else:
+                    amount, unit = None, None
+
+                price = int(product['price'].replace(',',''))
+                delivery_time = product['shipment']
+
+                if 'Orders outside of US' in delivery_time:
+                    stock_japan = False
+                else:
+                    stock_japan = True
+
+                products.append({'code' : code[i],
+                                 'amount' : amount,
+                                 'unit' : unit,
+                                 'price' : price,
+                                 'stock_japan' : stock_japan,
+                                 'aprox. delivery_time' : delivery_time})
+            except TypeError:
+                print(f'WARNING: Check product {code[i]}')
+    products_df = pd.DataFrame(products)
+    products_df.index = products_df['code']
+    products_df.drop(['code'], axis=1, inplace=True)
+    return products_df
+
+
+def get_smiles(lst,how_many,code):
+    smiles = []
+    for i in range(how_many):
+        for attribute in lst[i][code[i]]['attributes']:
+            if attribute['key'] == 'smiles string':
+                smile_string = attribute['values'][0]
+                smiles.append({'code' : code[i],
+                               'sigma_aldrich_smiles' : smile_string})
+    smiles_df = pd.DataFrame(smiles)
+    smiles_df.index = smiles_df['code']
+    smiles_df.drop(['code'], axis=1, inplace=True)
+    return smiles_df
+
+
 def main():
     with open("data_sigmaaldrich.json", 'r+', encoding='utf8') as data_f:
         entry = json.load(data_f)
 
-    print(get_unique_code(entry, 20))
-
-    '''    for product in entry[i][code[i]]['Available_products']:
-            quantity = re.findall(r'\d+',product['ID - quatity'])[-1]
-            units = product['ID - quatity'].split(quantity)[-1].strip()
-            print(i, ': ', quantity, units)
-    '''
-    #print(code)
+    num = 10
+    code = get_unique_code(entry, num)
+    #print(get_available_products(entry, num, code))
+    print(get_smiles(entry,num,code))
 
 if __name__ == "__main__":
     main()
@@ -49,23 +94,3 @@ if __name__ == "__main__":
 #    del entry[3] ##change IT to n!!!!!!
 #    data_f.seek(0)
 #    json.dump(entry, data_f, sort_keys=True, indent=4)
-
-'''
-with open('sigmaaldrich_products_urls.txt', 'r') as url_f:
-    urls_file = url_f.readlines()
-
-with open('sigmaaldrich_products_ALLurls.txt', 'r') as ALLurl_f:
-    ALLurls_file = ALLurl_f.readlines()
-
-indermediate_urls = [u.replace('\n', '') for u in urls_file]
-not_done = [prod for prod in indermediate_urls if prod not in lst]
-
-print('not done:', len(not_done),'done:', len(lst), 'indermediate_urls:', len(indermediate_urls))
-print('All products = Not Done + Done:', len(ALLurls_file), '=', len(not_done) + len(lst), '-> these numbers should be the same!')
-
-with open('sigmaaldrich_products_urls_intermediate.txt', 'w') as fp:
-    for item in not_done:
-        # write each item on a new line
-        fp.write("%s\n" % item)
-    print('Done')
-'''
