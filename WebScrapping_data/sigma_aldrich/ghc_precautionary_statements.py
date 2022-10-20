72ba4f8c50dd7d4ccb18951b5a681ebef3d5bb1c
stephanyvargas <yostephy@hotmail.com>
2022-10-20 09:02:30 +0900

    first commit

---
+++ WebScrapping_data/sigma_aldrich/ghc_precautionary_statements.py
@@ -0,0 +1,16 @@
+Hazard_statements = []
+
+H200 Unstable Explosive Explosives Unstable Explosive Danger P201, P202, P281, P372, P373, P380,
+P401, P501
+H201 Explosive; mass explosion hazard Explosives Div 1.1 Danger P210, P230, P240, P250, P280,
+P370+P380, P372, P373, P401, P501
+H202 Explosive; severe projection hazard Explosives Div 1.2 Danger P210, P230, P240, P250, P280,
+P370+P380, P372, P373, P401, P501
+H203 Explosive; fire, blast or projection hazard Explosives Div 1.3 Danger P210, P230, P240, P250, P280,
+P370+P380, P372, P373, P401, P501
+H204 Fire or projection hazard Explosives Div 1.4 Warning P210, P240, P250, P280, P370+P380,
+P372, P373, P374, P401, P501
+H205 May mass explode in fire Explosives Div 1.5 Danger P210, P230, P240, P250, P280,
+P370+P380, P372, P373, P401, P501
+H220 Extremely flammable gas Flammable gases Category 1 Danger P210, P377, P381, P403
+H221 Flammable gas Flammable gases Category 2 Warning P210, P377, P381, P403
